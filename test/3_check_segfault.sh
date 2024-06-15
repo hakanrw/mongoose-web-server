b304ec400fbe5d22ca831faa0f186040548c0f24
hakan <hakancandar@proton.me>
2024-06-15 22:29:55 +0300

    make segfault test more informative

--- test/3_check_segfault.sh
+++ test/3_check_segfault.sh
@@ -1,7 +1,17 @@
-for i in {1..128}; do
-	curl -X POST -H "Content-Type: application/json" -d '{"name":"user$i"}' http://localhost:8000/api/user -m 5 1>/dev/null 2>/dev/null
+printf "spamming users"
 
+for i in {1..128}; do
+	response=$(curl -X POST -H "Content-Type: application/json" -d '{"name":"user$i"}' http://localhost:8000/api/user -m 5 2>&1)
+	
 	if [ $? != 0 ]; then
+		echo # newline
+		echo "$response" # show error
 		exit 1 # fail on no-response
 	fi
+
+	if ((i % 4 == 0)); then 
+		printf . # progress bar
+	fi
 done
+
+echo # newline
